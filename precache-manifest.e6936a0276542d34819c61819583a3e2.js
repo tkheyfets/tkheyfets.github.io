7a2888f6895d5659eca7455b592275b8d90ef266
Timur Kheyfets <to@timur.io>
2020-02-10 11:54:04 -0500

    initial commit

---
+++ precache-manifest.e6936a0276542d34819c61819583a3e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c0104602757436f9218cba46d40f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e61c895903ce24f23208",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c26c10a24b10f828b98",
+    "url": "/static/js/2.6d661136.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.6d661136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61c895903ce24f23208",
+    "url": "/static/js/main.ed085d26.chunk.js"
+  },
+  {
+    "revision": "25246d43d2631f7308f4",
+    "url": "/static/js/runtime-main.a56052a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
